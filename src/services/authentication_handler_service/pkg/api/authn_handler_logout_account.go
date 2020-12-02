27ce97a4ab5f87107c968f0111acb2048612cd69
LensPlatform <yoanyombapro@gmail.com>
2020-12-02 00:03:53 -0500

    feat: enhance logout metrics handling
    Signed-off-by: LensPlatform <yoanyombapro@gmail.com>

--- src/services/authentication_handler_service/pkg/api/authn_handler_logout_account.go
+++ src/services/authentication_handler_service/pkg/api/authn_handler_logout_account.go
@@ -2,21 +2,34 @@ package api
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/BlackspaceInc/BlackspacePlatform/src/services/authentication_handler_service/pkg/constants"
 	"github.com/BlackspaceInc/BlackspacePlatform/src/services/authentication_handler_service/pkg/helper"
 )
 
 func (s *Server) logoutHandler(w http.ResponseWriter, r *http.Request) {
 	// hit authn log out endpoint and return
 	// we delete the session stored in the authentication service redis store
-	authErr := s.authnClient.Handler.LogOut()
-
-	if authErr != nil {
-		if _, err := helper.ProcessAggregatedErrors(w, authErr); err != nil {
-			s.logger.ErrorM(err, "failed to perform log out request")
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+	var (
+		begin = time.Now()
+		took  = time.Since(begin)
+		f = func() error {
+			if authErr := s.authnClient.Handler.LogOut(); authErr != nil {
+				s.logger.Info("status of logout", "err", authErr)
+				if _, err := helper.ProcessAggregatedErrors(w, authErr); err != nil {
+					return err
+				}
+			}
+			return nil
 		}
+	)
+
+	// TODO: perform this operation in a circuit breaker, and trace this
+	if err := s.RemoteOperationAndInstrument(f, constants.LOGOUT_ACCOUNT, &took); err != nil {
+		s.logger.ErrorM(err, "failed to perform logout account")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	s.JSONResponse(w, r, http.StatusOK)
