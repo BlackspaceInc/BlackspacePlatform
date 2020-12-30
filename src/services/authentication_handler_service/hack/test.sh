7acf3c00387db05d403a9b1fee5f20703e2101b8
LensPlatform <yoanyombapro@gmail.com>
2020-12-30 03:38:02 -0500

    feat: update CI
    Signed-off-by: LensPlatform <yoanyombapro@gmail.com>

--- src/services/authentication_handler_service/hack/test.sh
+++ src/services/authentication_handler_service/hack/test.sh
@@ -3,7 +3,7 @@
 set -e
 
 # wait for authentication_handler_service
-kubectl rollout status deployment/authentication_handler_service --timeout=3m
+kubectl rollout status deployment/authentication-handler-service --timeout=3m
 
 # test authentication_handler_service
-helm test authentication_handler_service
+helm test authentication-handler-service
